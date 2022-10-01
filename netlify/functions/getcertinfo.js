ad0d862aee7ecd72fba9660d4629952948b0d1a7
sumeshi <sum3sh1@protonmail.com>
2022-10-02 04:16:59 +0900

    fix: change query url to domain

--- netlify/functions/getcertinfo.js
+++ netlify/functions/getcertinfo.js
@@ -2,17 +2,25 @@ const https = require('https');
 const axios = require('axios');
 
 exports.handler = async (event, context) => {
-    const url = event.queryStringParameters.q;
+    const url = `https://${event.queryStringParameters.q}`;
     const requestConfig = {
         responseType: 'stream',
         httpsAgent: new https.Agent({
             rejectUnauthorized: false
         }),
     };
-    const response = await axios.get(url, requestConfig);
-    const cert = response.data.socket.getPeerCertificate();
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: cert }),
-    };
+
+    try {
+        const response = await axios.get(url, requestConfig);
+        const cert = response.data.socket.getPeerCertificate();
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: cert }),
+        };
+    } catch (e) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ errMessage: `${e.syscall} ${e.code}: ${e.hostname}` }),
+        };
+    }
 };
